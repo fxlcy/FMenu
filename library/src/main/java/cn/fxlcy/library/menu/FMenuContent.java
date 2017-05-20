079c5bb5c7b96e17a91cde68e6174d077ff63f00
Fxlcy <xiongchao@pv.cc>
2017-05-20 18:06:23 +0800

    none

--- library/src/main/java/cn/fxlcy/library/menu/FMenuContent.java
+++ library/src/main/java/cn/fxlcy/library/menu/FMenuContent.java
@@ -5,35 +5,66 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by fxlcy on 2017/5/20.
  */
 
-public class FMenuContent {
+public class FMenuContent<T> {
     private Context mContext;
     private RecyclerView mRecyclerView;
+    private List<T> mItems;
 
-    public FMenuContent(Context context) {
+    public FMenuContent(Context context, List<T> data) {
         mContext = context;
         mRecyclerView = new RecyclerView(context);
+        mItems = data;
     }
 
     void setDefaultAdapter() {
-        mRecyclerView.setAdapter(new RecyclerView.Adapter() {
+        mRecyclerView.setAdapter(new RecyclerView.Adapter<FMenuItemDefaultViewHolder>() {
             @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            public FMenuItemDefaultViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(mContext).inflate(viewType, parent, false);
+                return new FMenuItemDefaultViewHolder(itemView, viewType);
             }
 
             @Override
-            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            public void onBindViewHolder(FMenuItemDefaultViewHolder holder, int position) {
+                FMenuItem item = (FMenuItem) mItems.get(position);
 
+                final TextView text = holder.text;
+                text.setText(item.text);
+
+                if (item.textColor != null) {
+                    text.setTextColor(item.textColor);
+                }
+                if (item.textSize != -1) {
+                    text.setTextSize(item.textSize);
+                }
+                if (item.background != -1) {
+                    text.setBackgroundResource(item.background);
+                }
+
+                if (item.icon != -1) {
+                    final ImageView icon = holder.icon;
+                    icon.setImageResource(item.icon);
+                }
             }
 
             @Override
             public int getItemCount() {
-                return 0;
+                return mItems.size();
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                FMenuItem item = (FMenuItem) mItems.get(position);
+                return item.icon == -1 ? R.layout.item_context_menu_text : R.layout.item_context_menu_text_and_image;
             }
         });
     }
@@ -43,12 +74,24 @@ public class FMenuContent {
     }
 
 
-    final static class FMenuItemDefaultViewHolder extends RecyclerView.ViewHolder{
-        private TextView textView;
+    /**
+     * 默认视图的ViewHolder
+     */
+    private final static class FMenuItemDefaultViewHolder extends RecyclerView.ViewHolder {
+        TextView text;
+        ImageView icon;
 
-        public FMenuItemDefaultViewHolder(View itemView) {
+        FMenuItemDefaultViewHolder(View itemView, int itemType) {
             super(itemView);
 
+            if (itemType == R.layout.item_context_menu_text) {
+                text = (TextView) itemView;
+
+            } else if (itemType == R.layout.item_context_menu_text_and_image) {
+                icon = (ImageView) itemView.findViewById(R.id.iv_icon);
+                text = (TextView) itemView.findViewById(R.id.tv_content);
+
+            }
         }
     }
 }
--- library/src/main/java/cn/fxlcy/library/menu/FMenuItem.java
+++ library/src/main/java/cn/fxlcy/library/menu/FMenuItem.java
@@ -1,15 +1,19 @@
 package cn.fxlcy.library.menu;
 
 import android.content.res.ColorStateList;
+import android.support.annotation.DrawableRes;
+import android.widget.TextView;
 
 /**
  * Created by fxlcy on 2017/5/20.
  */
 
 final class FMenuItem {
-    private ColorStateList textColor;
-    private int textSize;
-    private int background;
-    private int icon;
-    private String text;
+    @DrawableRes
+    int background = -1;
+    @DrawableRes
+    int icon = -1;
+    ColorStateList textColor;
+    int textSize = -1;
+    String text;
 }
